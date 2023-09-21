d1fb3046d5369f13b8b059962c28e2df6f0bfc46
ioansimion <brokenparticles@gmail.com>
2023-09-21 19:34:51 +0300

    Separated code into functions

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let hints = (await chrome.storage.sync.get('hints')).hints || [];
+let domains;
 let currentURL = new URL((await chrome.tabs.query({ active: true, lastFocusedWindow: true }))[0].url).hostname;
+const buttonContainer = document.querySelector('#button-container');
+
+function updateDomains() {
+    domains = hints.map(item => item.domain);
+}
 
 function updateSaved() {
     const savedHints = document.querySelector('#saved-hints');
@@ -21,60 +27,85 @@ function updateSaved() {
     }
 }
 
-function createHint(domain, hint) {
-    hints.push({ domain, hint });
+function createHint(hint) {
+    hints.push({ domain: currentURL, hint });
     chrome.storage.sync.set({ hints });
+    updateDomains();
     updateSaved();
 }
 
-document.querySelector('#add-current-page-button').addEventListener('click', () => {
-    // This function has to:
-    // Check if a url is in the object list
-    // If true, then display a warning message with two options
-    // Option 1: Proceed with creating a new entry in the storage
-    // Option 2: Cancel everything
-    // If false, then: Proceed with creating a new entry in the storage
-    if (hints.includes(currentURL)) {
-        // Item already exists
-        const warning = document.querySelector('#warning');
-        const warningHTML = `
-            <p class="lead">You already saved this page. Continue?</p>
-            <button id="yes-button" class="btn btn-success">Yes</button>
-            <button id="no-button" class="btn btn-danger">No</button>`;
+function displayForm() {
+    buttonContainer.innerHTML = '';
 
-        warning.innerHTML = warningHTML;
-        warning.classList.add('mt-3');
+    const form = document.querySelector('#hint-form');
+    const formHTML = `
+        <input id="hint-input" class="form-control" type="text" placeholder="Type your hint">
+        <button id="submit-button" class="btn btn-outline-light custom-button">Submit</button>`;
 
-        document.querySelector('#yes-button').addEventListener('click', () => {
-            createHint(currentURL, hintInput.value);
+    form.innerHTML = formHTML;
 
-            warning.innerHTML = '';
-            warning.classList.remove('mt-3');
-        });
+    const input = document.querySelector('#hint-input');
+    input.classList.add('mb-3');
+    input.focus();
 
-        document.querySelector('#no-button').addEventListener('click', () => {
-            warning.innerHTML = '';
-            warning.classList.remove('mt-3');
-        });
+    document.querySelector('#submit-button').addEventListener('click', () => {
+        if (input.value) {
+            createHint(input.value);
 
-    } else {
-        const hintForm = document.querySelector('#hint-form');
-        const hintHTML = `
-            <input id="hint-input" class="form-control" type="text" placeholder="Type your hint">
-            <button id="submit-button" class="btn btn-outline-light custom-button">Submit</button>`;
+            form.innerHTML = '';
 
-        hintForm.innerHTML = hintHTML;
-        hintForm.classList.add('mt-3');
+            updateButtonContainer();
+        } else {
+            // Display an error message
+        }
+    });
+}
 
-        const hintInput = document.querySelector('#hint-input');
-        hintInput.classList.add('mb-3');
+function displayWarning() {
+    buttonContainer.innerHTML = '';
 
-        document.querySelector('#submit-button').addEventListener('click', () => {
-            if (hintInput.value) {
-                createHint(currentURL, hintInput.value);
-            } else {
-                // Display an error message
-            }
-        });
-    }
-});
+    const warning = document.querySelector('#warning');
+    const warningHTML = `
+        <p class="lead">You already saved this page. Continue?</p>
+        <button id="yes-button" class="btn btn-success">Yes</button>
+        <button id="no-button" class="btn btn-danger">No</button>`;
+
+    warning.innerHTML = warningHTML;
+
+    document.querySelector('#yes-button').addEventListener('click', () => {
+        warning.innerHTML = '';
+
+        displayForm();
+    });
+
+    document.querySelector('#no-button').addEventListener('click', () => {
+        warning.innerHTML = '';
+
+        updateButtonContainer();
+    });
+}
+
+function updateButtonContainer() {
+    buttonContainer.innerHTML = `
+        <button id="add-hint-button" class="btn btn-lg btn-outline-light custom-button">
+            Add this page
+        </button>`;
+
+    document.querySelector('#add-hint-button').addEventListener('click', () => {
+        // This function has to:
+        // Check if a url is in the object list
+        // If true, then display a warning message with two options
+        // Option 1: Proceed with creating a new entry in the storage
+        // Option 2: Cancel everything
+        // If false, then: Proceed with creating a new entry in the storage
+        if (domains.includes(currentURL)) {
+            displayWarning();
+        } else {
+            displayForm();
+        }
+    });
+}
+
+updateDomains();
+updateButtonContainer();
+updateSaved();
